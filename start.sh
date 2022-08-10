a31f42a7719ff649df1f5860e4640a1d26c651cc
wangvps <99784662+wangvps@users.noreply.github.com>
2022-08-11 02:36:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ SessionSecret = gKgAahH5nPWJZ2R3Ozz6ZSTIEOCUN0veppJ8ItYsGDaL9YKqWEGZFEt5idQVKMw2
 HashIDSalt = TkfGFcKECYZa5NQZGH4fv0OEV6jnUMiDywpf8Xq9J7aaqFYYcb1nvi5MWybYdywK
 
 [Redis]
-Server = ${SERVER_HOST}:6379
+Server = ${SERVER_HOST}
 Password = ${PASSWORD}
 DB = 0
 EOF
