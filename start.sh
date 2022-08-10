2377683e672e6f9ed9f0b8fe13b6597f43b00817
wangvps <99784662+wangvps@users.noreply.github.com>
2022-08-11 02:32:55 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cat < EOF >> /conf.ini
+[System]
+Debug = false
+Mode = master
+Listen = :5212
+SessionSecret = gKgAahH5nPWJZ2R3Ozz6ZSTIEOCUN0veppJ8ItYsGDaL9YKqWEGZFEt5idQVKMw2
+HashIDSalt = TkfGFcKECYZa5NQZGH4fv0OEV6jnUMiDywpf8Xq9J7aaqFYYcb1nvi5MWybYdywK
+
+[Redis]
+Server = ${SERVER_HOST}:6379
+Password = ${PASSWORD}
+DB = 0
+EOF
+
